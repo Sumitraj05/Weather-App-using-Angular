0e53c8c626b8c877b31df89273a733df8689ebf3
Sumit Raj Tiwari <137185097+Sumitraj05@users.noreply.github.com>
2024-07-30 00:05:20 +0530

    Initial commit

---
+++ Weather_App/src/app/Environment/EnvironmentVariables.ts
@@ -0,0 +1,16 @@
+export const EnvironmentalVariables = {
+    production:false,
+
+    //Base Urls to access the end points using the http client get method
+    weatherApiLocationBaseURL:'https://weather338.p.rapidapi.com/locations/search?',
+    weatherApiForecastBaseURL:'https://weather338.p.rapidapi.com/weather/forecast?',
+    
+    //Variables for API key name and value
+    xRapidApiKeyName:'x-rapidapi-key',
+    xRapidApikeyValue:'28ca5a509dmsh4d92aeba165a6f3p1d9401jsneca73232e72e',
+
+    //Variables for host name and value
+    xRapidApiHostName:'x-rapidapi-host',
+    xRapidApiHostValue:'weather338.p.rapidapi.com'
+    
+}
